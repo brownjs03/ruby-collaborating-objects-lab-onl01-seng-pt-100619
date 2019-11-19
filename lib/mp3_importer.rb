b8974336ccc596109fe9e1e3b160394c4099b9f3
Jeffrey Brown <brownjs03@gmail.com>
2019-11-19 05:20:49 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -7,18 +7,15 @@ attr_accessor :path
   end
 
   def files
-
     loaded_files = Dir.glob("#{path}/*.mp3")
     loaded_files.collect do |file|
       file.gsub("#{path}/", "")
     end
-end
+  end
 
   def import
     self.files.each do |file|
       Song.new_by_filename(file)
     end
   end
-
-
 end
