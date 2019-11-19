fc57d44b0332927b508b66035f4e6e20a79a30ff
Jeffrey Brown <brownjs03@gmail.com>
2019-11-19 05:17:54 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,28 +1,41 @@
-require 'pry'
-class Artist 
-  
-  attr_accessor :name 
-  
+class Artist
+
+  attr_accessor :name
+
   @@all = []
-  
+
   def initialize(name)
-    @name = name 
-    @@all << self 
-  end 
-  
-  def self.all 
-    @@all 
-  end 
-  
-  def songs 
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def songs
     Song.all.select {|song| song.artist == self}
-  end 
-  
+  end
+
   def add_song(song)
-    song.artist = self 
-  end 
-  
+    song.artist = self
+  end
+
+  def self.find_by_name(name)
+    self.all.find {|artist| artist.name == name}
+  end
+
   def self.find_or_create_by_name(name)
-  end 
-  
+    if self.find_by_name(name)
+      return self.find_by_name(name)
+    else
+      self.new(name)
+    end
+  end
+
+  def print_songs
+    self.songs.each do |song|
+      puts song.name
+    end
+  end
 end 
